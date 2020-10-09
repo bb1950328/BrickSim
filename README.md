976061ecd035445972b2a186e0b22e379ed458b4
bb1950328 <bb1950328@gmail.com>
2020-10-09 17:09:11 +0200

    small changes

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 - select object on mouse click
 - add camera pan
 - make ldraw directory listing faster on windows
+- customizable dpi / dpi detection
--- src/gui.cpp
+++ src/gui.cpp
@@ -68,11 +68,6 @@ void loopGui(GLFWwindow* window) {
 
     // Rendering
     ImGui::Render();
-    int display_w, display_h;
-    glfwGetFramebufferSize(window, &display_w, &display_h);
-    //glViewport(0, 0, display_w, display_h);
-    //glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-    //glClear(GL_COLOR_BUFFER_BIT);
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 

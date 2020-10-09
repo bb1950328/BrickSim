05c3ee52c113e4cde3494431666f51e3d16f220a
bb1950328 <bb1950328@gmail.com>
2020-10-09 16:35:55 +0200

    enabled docking, small bugfix in mouse event handling

--- src/gui.cpp
+++ src/gui.cpp
@@ -12,6 +12,7 @@ void initGui(GLFWwindow* window) {
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     // Setup Platform/Renderer bindings
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");

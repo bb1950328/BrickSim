d147fe5d035a62d5660b21fab44c3faf09f8b9e7
bb1950328 <bb1950328@gmail.com>
2021-01-07 17:10:03 +0100

    reordered and grouped functions in util.h

--- src/helpers/util.h
+++ src/helpers/util.h
@@ -11,6 +11,7 @@
 #include <filesystem>
 #include <ostream>
 #include <glm/glm.hpp>
+#include "../types.h"
 
 namespace util {
     namespace {
@@ -27,39 +28,55 @@ namespace util {
     const char PATH_SEPARATOR_FOREIGN = '\\';
 #endif
 
+    // path functions
     std::string extendHomeDir(const std::string &input);
     std::filesystem::path extendHomeDirPath(const std::string &input);
-    std::string trim(const std::string& input);
+    std::filesystem::path withoutBasePath(const std::filesystem::path& path, const std::filesystem::path& basePath);
     std::string pathjoin(const std::list<std::string>& parts);
+
+    //string functions
+    std::string trim(const std::string& input);
     std::string asLower(const std::string& string);
     bool endsWith(std::string const &fullString, std::string const &ending);
     bool startsWith(std::string const &fullString, std::string const &start);
-    void coutMat4(glm::mat4 mat);
     void replaceAll(std::string& str, const std::string& from, const std::string& to);
     std::string replaceChar(const std::string& str, char from, char to);
+    std::string formatBytesValue(size_t bytes);
+    std::string fileToString(const std::filesystem::path& path);
+    bool containsIgnoreCase(const std::string& full, const std::string& sub);
+    bool equalsAlphanum(std::string a, std::string b);
+
+    //number functions
     unsigned long gcd(unsigned long a, unsigned long b);
     unsigned long lcm(unsigned long a, unsigned long b);
+
+    //os functions
+    void openDefaultBrowser(const std::string& link);
+    std::vector<std::string> getSystemInfo();
+
+    //vector/glm functions
+    void coutMat4(glm::mat4 mat);
     float biggestValue(glm::vec2 vector);
     float biggestValue(glm::vec3 vector);
     float biggestValue(glm::vec4 vector);
     float vectorSum(glm::vec2 vector);
     float vectorSum(glm::vec3 vector);
     float vectorSum(glm::vec4 vector);
-    void openDefaultBrowser(const std::string& link);
-    glm::vec3 triangleCentroid(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3);
-    glm::vec3 quadrilateralCentroid(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, const glm::vec3& p4);
-    bool doesTransformationInverseWindingOrder(const glm::mat4& transformation);
-    glm::vec3 convertIntToColorVec3(unsigned int value);
-    unsigned int getIntFromColor(unsigned char red, unsigned char green, unsigned char blue);
-    std::string formatBytesValue(size_t bytes);
-    std::string fileToString(const std::filesystem::path& path);
     glm::vec2 minForEachComponent(const glm::vec2& a, const glm::vec2& b);
     glm::vec3 minForEachComponent(const glm::vec3& a, const glm::vec3& b);
     glm::vec4 minForEachComponent(const glm::vec4& a, const glm::vec4& b);
 
-    class HSVcolor;
+    //geometry functions
+    glm::vec3 triangleCentroid(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3);
+    glm::vec3 quadrilateralCentroid(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, const glm::vec3& p4);
+    bool doesTransformationInverseWindingOrder(const glm::mat4& transformation);
 
-    typedef unsigned char color_component_t;
+
+    //color functions
+    glm::vec3 convertIntToColorVec3(unsigned int value);
+    unsigned int getIntFromColor(unsigned char red, unsigned char green, unsigned char blue);
+
+    class HSVcolor;
 
     class RGBcolor {
     public:
@@ -86,6 +103,8 @@ namespace util {
         [[nodiscard]] glm::vec3 asGlmVector() const;
     };
 
+
+    // texture/image functions
     struct TextureLoadResult {
         unsigned int textureId;
         int width;
@@ -93,19 +112,17 @@ namespace util {
         int nrChannels;
     };
 
-    std::vector<std::string> getSystemInfo();
-    bool memeqzero(const void *data, size_t length);
     std::string translateBrickLinkColorNameToLDraw(std::string colorName);
     std::string translateLDrawColorNameToBricklink(std::string colorName);
-    bool equalsAlphanum(std::string a, std::string b);
-    std::filesystem::path withoutBasePath(const std::filesystem::path& path, const std::filesystem::path& basePath);
     bool writeImage(const char* path, unsigned char* pixels, unsigned int width, unsigned int height, int channels=3);
-    bool containsIgnoreCase(const std::string& full, const std::string& sub);
+
     TextureLoadResult loadTextureFromFile(const std::filesystem::path& image);
     TextureLoadResult loadTextureFromMemory(const unsigned char* fileData, unsigned int dataSize);
 
     bool isStbiFlipVertically();
     void setStbiFlipVertically(bool value);
+
+    bool memeqzero(const void *data, size_t length);
 
     /**
      * @param url
--- src/types.h
+++ src/types.h
@@ -6,5 +6,6 @@
 #define BRICKSIM_TYPES_H
 
 typedef unsigned char layer_t;
+typedef unsigned char color_component_t;
 
 #endif //BRICKSIM_TYPES_H

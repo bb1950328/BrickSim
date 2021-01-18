f984bd8bc20541a2b59ca36b8f8470f5a40e4996
bb1950328 <bb1950328@gmail.com>
2021-01-18 08:12:32 +0100

    trying to get the linux build to run again 5

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -39,7 +39,7 @@ fi
 if [[ "$OS" == "linux" ]]; then
   echo "Installing packages using apt-get..."
   sudo apt-get update
-  sudo apt-get install mesa-utils freeglut3-dev libxinerama-dev libxrandr-dev libxcursor-dev libxi-dev libglew-dev libcurl4-openssl-dev libglm-dev libzip-dev gcc-10 g++-10 libglew ## glew-utils libxrandr-dev
+  sudo apt-get install mesa-utils freeglut3-dev libxinerama-dev libxrandr-dev libxcursor-dev libxi-dev libglew-dev libcurl4-openssl-dev libglm-dev libzip-dev gcc-10 g++-10 libglew xorg-dev ## glew-utils libxrandr-dev
   echo "packages installed."
 fi
 

58f2ffb04347372a0a1313d3d4cb11db5e658281
bb1950328 <bb1950328@gmail.com>
2021-03-28 14:44:22 +0200

    use threadsafe localtime_r instead of std::localtime

--- src/latest_log_messages_tank.h
+++ src/latest_log_messages_tank.h
@@ -58,8 +58,10 @@ namespace latest_log_messages_tank {
         void sink_it_(const spdlog::details::log_msg &msg) override {
             constexpr auto timeBufSize = sizeof("12:34:56.789");
             static char timeBuf[timeBufSize];
-            const time_t timestamp = std::chrono::system_clock::to_time_t(msg.time);
-            std::strftime(timeBuf, timeBufSize, "%H:%M:%S", std::localtime(&timestamp));
+            const time_t timestamp = spdlog::log_clock::to_time_t(msg.time);
+            tm tmpTm{};
+            localtime_r(&timestamp, &tmpTm);
+            std::strftime(timeBuf, timeBufSize, "%H:%M:%S", &tmpTm);
             const auto timeMs = std::chrono::time_point_cast<std::chrono::milliseconds>(msg.time).time_since_epoch().count();
             snprintf(&timeBuf[8], 5, ".%03ld", timeMs % 1000);
             std::string message = msg.payload.data();

bb1950328/BrickSim

commit 09c5dd44656e63f6cd85d2e16bc9b55772d80f99

working on overlay_2d::

src/overlay_2d.cpp
@@ -15,6 +15,14 @@ namespace overlay2d {
         collection.lock()->verticesHaveChanged(shared_from_this());
     }
 
+    void Element::addToCollection() {
+        collection.lock()->addElement(shared_from_this());
+    }
+
+    void Element::removeFromCollection() {
+        collection.lock()->removeElement(shared_from_this());
+    }
+
     void ElementCollection::verticesHaveChanged(const std::shared_ptr<Element>& changedElement) {
         changedElements.insert(changedElement);
     }
@@ -24,18 +32,34 @@ namespace overlay2d {
         std::vector<VertexRange> changedRanges;
         const auto &firstVertex = vertices.begin();
         for (const auto &elem : changedElements) {
-            auto &range = vertexRanges.find(elem)->second;
+            auto vertexRangesIt = vertexRanges.find(elem);
+            const bool elementIsNew = vertexRangesIt == vertexRanges.end();
+            VertexRange &range = elementIsNew ? vertexRanges[elem] = {} : vertexRangesIt->second;
+            const bool elementIsDeleted = elements.find(elem) == elements.end();
             const auto newVertexCount = elem->getVertexCount();
-            if (range.count != newVertexCount) {
-                needToRewriteEverything = true;
-                vertices.erase(firstVertex+range.start, firstVertex+range.start+range.count-1);
+            const bool elementVertexCountChanged = range.count != newVertexCount;
+
+            const bool needToAppendAtEnd = elementVertexCountChanged || elementIsNew || elementIsDeleted;
+            needToRewriteEverything |= needToAppendAtEnd;
+            
+            if (elementVertexCountChanged || elementIsDeleted) {
+                vertices.erase(firstVertex + range.start, firstVertex + range.start + range.count - 1);
+
+                //adjust ranges after current
+                for (auto &item : vertexRanges) {
+                    if (item.second.start > range.start) {
+                        item.second.count -= range.count;
+                    }
+                }
+            }
+            if (needToAppendAtEnd) {
                 const auto firstToWrite = vertices.end();
                 vertices.resize(vertices.size()+newVertexCount);
                 elem->writeVertices(firstToWrite);
-                range.start = firstToWrite-firstVertex;
+                range.start = firstToWrite - firstVertex;
                 range.count = newVertexCount;
             } else {
-                elem->writeVertices(firstVertex+range.count);
+                elem->writeVertices(firstVertex + range.count);
                 if (!needToRewriteEverything) {
                     changedRanges.push_back(range);
                 }
@@ -53,6 +77,7 @@ namespace overlay2d {
                 }
             }
         });
+        changedElements.clear();
     }
 
     ElementCollection::ElementCollection() { // NOLINT(cppcoreguidelines-pro-type-member-init)
@@ -79,6 +104,22 @@ namespace overlay2d {
             glDeleteVertexArrays(1, &vao);
             glDeleteBuffers(1, &vbo);
         });
+    }
+
+    void ElementCollection::draw() {
+        updateVertices();
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+    }
+
+    void ElementCollection::addElement(const std::shared_ptr<Element> &element) {
+        elements.insert(element);
+        verticesHaveChanged(element);
+    }
+
+    void ElementCollection::removeElement(const std::shared_ptr<Element> &element) {
+        elements.erase(element);
+        verticesHaveChanged(element);
     }
 
     void generateVerticesForLine(std::vector<Vertex>::iterator& firstVertexLocation, glm::vec2 start, glm::vec2 end, float width, util::RGBcolor color) {
@@ -161,5 +202,21 @@ namespace overlay2d {
     }
     constexpr unsigned int getVertexCountForQuad() {
         return 6;
+    }
+
+    bool LineElement::isPointInside(glm::vec2 point) {
+        return util::calculateDistanceOfPointToLine(start, end, point) <= width/2;
+    }
+
+    unsigned int LineElement::getVertexCount() {
+        return getVertexCountForLine();
+    }
+
+    void LineElement::writeVertices(std::vector<Vertex>::iterator firstVertexLocation) {
+        generateVerticesForLine(firstVertexLocation, start, end, width, color);
+    }
+
+    LineElement::LineElement(std::weak_ptr<ElementCollection> collection, glm::vec2 start, glm::vec2 end, float width, util::RGBcolor color) : Element(collection), start(start), end(end), width(width), color(color) {
+
     }
 }

src/overlay_2d.h
@@ -29,7 +29,7 @@ namespace overlay2d {
 
     class ElementCollection {
     private:
-        std::vector<std::shared_ptr<Element>> elements;
+        std::set<std::shared_ptr<Element>> elements;
         std::set<std::shared_ptr<Element>> changedElements;
         std::map<std::shared_ptr<Element>, VertexRange> vertexRanges;
         std::vector<Vertex> vertices;
@@ -39,6 +39,10 @@ namespace overlay2d {
         void updateVertices();
     public:
         void verticesHaveChanged(const std::shared_ptr<Element>& changedElement);
+        void draw();
+
+        void addElement(const std::shared_ptr<Element>& element);
+        void removeElement(const std::shared_ptr<Element>& element);
 
         ElementCollection();
         ElementCollection & operator=(ElementCollection&) = delete;
@@ -53,9 +57,23 @@ namespace overlay2d {
         void verticesHaveChanged();
     public:
         explicit Element(std::weak_ptr<ElementCollection> collection);
-        virtual bool isPointInside(glm::usvec2 point) = 0;
+        void addToCollection();
+        void removeFromCollection();
+        virtual bool isPointInside(glm::vec2 point) = 0;
         virtual unsigned int getVertexCount() = 0;
         virtual void writeVertices(std::vector<Vertex>::iterator firstVertexLocation) = 0;
+    };
+
+    class LineElement: public Element {
+    private:
+        glm::vec2 start, end;
+        float width;
+        util::RGBcolor color;
+    public:
+        LineElement(std::weak_ptr<ElementCollection> collection, glm::vec2 start, glm::vec2 end, float width, util::RGBcolor color);
+        bool isPointInside(glm::vec2 point) override;
+        unsigned int getVertexCount() override;
+        void writeVertices(std::vector<Vertex>::iterator firstVertexLocation) override;
     };
 
     void generateVerticesForLine(std::vector<Vertex>::iterator& firstVertexLocation, glm::vec2 start, glm::vec2 end, float width, util::RGBcolor color);

src/scene.cpp
@@ -276,6 +276,9 @@ void Scene::updateImage() {
                 meshCollection.drawOptionalLineGraphics(layer);
             }
 
+            shaders::get(shaders::OVERLAY).use();
+            overlayCollection.draw();
+
             glBindFramebuffer(GL_FRAMEBUFFER, 0);
             glUseProgram(0);
             glFinish();

CMakeLists.txt
@@ -39,6 +39,7 @@ if (UNIX)# todo check if this is necessary
         target_link_libraries(BrickSim ${X11_LIBRARIES})
     endif ()
 endif ()
+target_compile_definitions(BrickSim PUBLIC GLFW_INCLUDE_NONE)
 
 # GLUT
 find_package(GLUT REQUIRED)

src/controller.cpp
@@ -361,6 +361,7 @@ namespace controller {
         openFile("~/Downloads/arocs.mpd");
         //openFile("3001.dat");
 
+
         while (!(glfwWindowShouldClose(window) || userWantsToExit)) {
             copyMainloopTimePoints();
             if (foregroundTasks.empty() && backgroundTasks.empty() && thumbnailGenerator->renderQueueEmpty() && glfwGetWindowAttrib(window, GLFW_FOCUSED) == 0) {

src/helpers/util.cpp
@@ -544,4 +544,11 @@ namespace util {
             string[i] = toupper(string[i]);
         }
     }
+
+    float calculateDistanceOfPointToLine(const glm::vec2 &line_start, const glm::vec2 &line_end, const glm::vec2 &point) {
+        //https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Line_defined_by_two_points
+        float numerator = std::abs((line_end.x - line_start.x) * (line_start.y - point.y) - (line_start.x - point.x) * (line_end.y - line_start.y));
+        float denominator = std::sqrt(std::pow(line_end.x - line_start.x, 2.0f) + std::pow(line_end.y - line_start.y, 2.0f));
+        return numerator / denominator;
+    }
 }

src/helpers/util.h
@@ -63,6 +63,7 @@ namespace util {
     glm::vec3 triangleCentroid(const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3);
     glm::vec3 quadrilateralCentroid(const glm::vec3 &p1, const glm::vec3 &p2, const glm::vec3 &p3, const glm::vec3 &p4);
     bool doesTransformationInverseWindingOrder(const glm::mat4 &transformation);
+    float calculateDistanceOfPointToLine(const glm::vec2& line_start, const glm::vec2& line_end, const glm::vec2& point);
 
 
     //color functions

commit 0e5a811b37882e005ccd02c6dc4b02974b363922

fix bug introduced in 09c5dd44656e63f6cd85d2e16bc9b55772d80f99

src/overlay_2d.cpp
@@ -122,6 +122,10 @@ namespace overlay2d {
         verticesHaveChanged(element);
     }
 
+    bool ElementCollection::hasElements() {
+        return !elements.empty();
+    }
+
     void generateVerticesForLine(std::vector<Vertex>::iterator& firstVertexLocation, glm::vec2 start, glm::vec2 end, float width, util::RGBcolor color) {
         // 1----------------------------------2
         // |                                  |

src/overlay_2d.h
@@ -48,6 +48,7 @@ namespace overlay2d {
         ElementCollection & operator=(ElementCollection&) = delete;
         ElementCollection(const ElementCollection&) = delete;
         virtual ~ElementCollection();
+        bool hasElements();
     };
 
     class Element : std::enable_shared_from_this<Element> {

src/scene.cpp
@@ -125,6 +125,7 @@ unsigned int Scene::getSelectionPixel(framebuffer_size_t x, framebuffer_size_t y
 
     if (selectionImageUpToDate) {
         controller::executeOpenGL([&]() {
+            glUseProgram(0);
             glBindFramebuffer(GL_FRAMEBUFFER, selection->getFBO());
             glReadPixels(x, (selection->getSize().y - y), 1, 1, GL_RGB, GL_UNSIGNED_BYTE, middlePixel);
             glBindFramebuffer(GL_FRAMEBUFFER, 0);
@@ -276,8 +277,11 @@ void Scene::updateImage() {
                 meshCollection.drawOptionalLineGraphics(layer);
             }
 
-            shaders::get(shaders::OVERLAY).use();
-            overlayCollection.draw();
+            if (overlayCollection.hasElements()) {
+                glClear(GL_DEPTH_BUFFER_BIT);
+                shaders::get(shaders::OVERLAY).use();
+                overlayCollection.draw();
+            }
 
             glBindFramebuffer(GL_FRAMEBUFFER, 0);
             glUseProgram(0);
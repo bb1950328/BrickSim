84657ec40f5563b9856caa1e09896c89b51fd8b7
bb1950328 <bb1950328@gmail.com>
2021-04-25 00:28:22 +0200

    add 32bit linux build try 7

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -40,8 +40,8 @@ fi
 if [[ "$OS" == "linux" ]]; then
   echo "Installing packages using apt-get..."
   sudo apt-get update
-  sudo apt-get install -y build-essential mesa-utils gcc-10 g++-10 gcc-multilib g++-multilib cmake libxinerama-dev \
-                          libxrandr-dev libxcursor-dev libxi-dev
+  sudo apt-get install -y build-essential gcc-10 g++-10 gcc-multilib g++-multilib gcc-10-multilib g++-10-multilib \
+                          cmake mesa-utils libxinerama-dev libxrandr-dev libxcursor-dev libxi-dev
 
   if [ "$install_32bit_packages" ]; then
     sudo apt-get install -y libgl1-mesa-dev:i386 libglu1-mesa-dev:i386 freeglut3-dev:i386 \

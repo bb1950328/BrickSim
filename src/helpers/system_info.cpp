6f87426199bf61df8bc4c1909f6ba83fb72231bc
bb1950328 <bb1950328@gmail.com>
2021-05-27 01:37:24 +0200

    remove custom cpuid code -> replace it with library add magic_enum library improve system_info.cpp a bit

--- src/helpers/system_info.cpp
+++ src/helpers/system_info.cpp
@@ -9,9 +9,13 @@
 #include <stb_image.h>
 #include <spdlog/spdlog.h>
 
+#include <cpuinfo.h>
+#include <magic_enum.hpp>
+
 namespace system_info {
-    std::vector<std::pair<const char *, std::string>> getSystemInfo() {
-        std::vector<std::pair<const char *, std::string>> result;
+
+    std::vector<std::pair<std::string, std::string>> getSystemInfo() {
+        std::vector<std::pair<std::string, std::string>> result;
         const GLubyte *vendor;
         const GLubyte *renderer;
         controller::executeOpenGL([&]() {
@@ -25,148 +29,28 @@ namespace system_info {
         result.emplace_back("Git Commit Hash:", constants::gitCommitHash);
         result.emplace_back("Dear ImGui Version:", ImGui::GetVersion());
         result.emplace_back("Libcurl Version:", LIBCURL_VERSION);
-        result.emplace_back("Spdlog Version:",
-                            std::to_string(SPDLOG_VER_MAJOR) + '.' + std::to_string(SPDLOG_VER_MINOR) + '.' + std::to_string(SPDLOG_VER_PATCH));
+        result.emplace_back("Spdlog Version:", fmt::format("{}.{}.{}", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH));
         result.emplace_back("STBI Version:", std::to_string(STBI_VERSION));
-        result.emplace_back("GLFW Version:",
-                            std::to_string(GLFW_VERSION_MAJOR) + "." + std::to_string(GLFW_VERSION_MINOR) + "." + std::to_string(GLFW_VERSION_REVISION));
+        result.emplace_back("GLFW Version:", fmt::format("{}.{}.{}", GLFW_VERSION_MAJOR, GLFW_VERSION_MINOR, GLFW_VERSION_REVISION));
 
-        result.emplace_back("sizeof(void*):", std::to_string(sizeof(void *)) + " Bytes or " + std::to_string(sizeof(void *) * 8) + " Bits");
-        result.emplace_back("sizeof(char):", std::to_string(sizeof(char)) + " Bytes or " + std::to_string(sizeof(char) * 8) + " Bits");
-        result.emplace_back("sizeof(int):", std::to_string(sizeof(int)) + " Bytes or " + std::to_string(sizeof(int) * 8) + " Bits");
-        result.emplace_back("sizeof(long):", std::to_string(sizeof(long)) + " Bytes or " + std::to_string(sizeof(long) * 8) + " Bits");
-        result.emplace_back("sizeof(float):", std::to_string(sizeof(float)) + " Bytes or " + std::to_string(sizeof(float) * 8) + " Bits");
-        result.emplace_back("sizeof(double):", std::to_string(sizeof(double)) + " Bytes or " + std::to_string(sizeof(double) * 8) + " Bits");
+        result.emplace_back("sizeof(void*):", fmt::format("{} Bytes or {} bits", sizeof(void *), sizeof(void *) * 8));
+        result.emplace_back("sizeof(char):", fmt::format("{} Bytes or {} bits", sizeof(char), sizeof(char) * 8));
+        result.emplace_back("sizeof(int):", fmt::format("{} Bytes or {} bits", sizeof(int), sizeof(int) * 8));
+        result.emplace_back("sizeof(long):", fmt::format("{} Bytes or {} bits", sizeof(long), sizeof(long) * 8));
+        result.emplace_back("sizeof(float):", fmt::format("{} Bytes or {} bits", sizeof(float), sizeof(float) * 8));
+        result.emplace_back("sizeof(double):", fmt::format("{} Bytes or {} bits", sizeof(double), sizeof(double) * 8));
 
-        CPUInfo cpuInfo;
-        result.emplace_back("CPU Vendor:", cpuInfo.vendor());
-        result.emplace_back("CPU Model:", cpuInfo.model());
-        result.emplace_back("Number of CPU cores:", std::to_string(cpuInfo.cores()));
-        result.emplace_back("Number logical CPUs:", std::to_string(cpuInfo.logicalCpus()));
-        std::string cpuFeatures;
-        std::map<const char*, bool> features = {
-                {"SSE", cpuInfo.isSSE()},
-                {"SSE2", cpuInfo.isSSE2()},
-                {"SSE3", cpuInfo.isSSE3()},
-                {"SSE41", cpuInfo.isSSE41()},
-                {"SSE42", cpuInfo.isSSE42()},
-                {"AVX", cpuInfo.isAVX()},
-                {"AVX2", cpuInfo.isAVX2()},
-                {"HyperThreaded", cpuInfo.isHyperThreaded()},
-        };
-        bool first = true;
-        for (const auto &item : features) {
-            if (item.second) {
-                if (first) {
-                    first = false;
-                } else {
-                    cpuFeatures += ' ';
-                }
-                cpuFeatures += item.first;
-            }
-        }
-        result.emplace_back("CPU Features:", cpuFeatures);
+        cpuinfo_initialize();
+
+        const cpuinfo_processor *const processor = cpuinfo_get_current_processor();
+        const cpuinfo_cluster *cluster = processor->cluster;
+        result.emplace_back("CPU vendor:", magic_enum::enum_name(cluster->vendor).substr(15));
+        result.emplace_back("CPU package name: ", cluster->package->name);
+        result.emplace_back("CPU microarchitecture code: ", fmt::format("{:#08x}", cluster->uarch));
 
         result.emplace_back("ImGui::GetFontSize():", std::to_string(ImGui::GetFontSize()));
         result.emplace_back("ImGui::GetWindowDpiScale():", std::to_string(ImGui::GetWindowDpiScale()));
 
         return result;
-    }
-
-    CPUID::CPUID(unsigned int funcId, unsigned int subFuncId) {
-#ifdef _WIN32
-        __cpuidex((int *) regs, (int) funcId, (int) subFuncId);
-
-#else
-        asm volatile
-            ("cpuid" : "=a" (regs[0]), "=b" (regs[1]), "=c" (regs[2]), "=d" (regs[3])
-             : "a" (funcId), "c" (subFuncId));
-        // ECX is set to zero for CPUID function 4
-#endif
-    }
-
-    CPUInfo::CPUInfo() {
-        // Get vendor name EAX=0
-        CPUID cpuID0(0, 0);
-        uint32_t HFS = cpuID0.EAX();
-        mVendorId += std::string((const char *) &cpuID0.EBX(), 4);
-        mVendorId += std::string((const char *) &cpuID0.EDX(), 4);
-        mVendorId += std::string((const char *) &cpuID0.ECX(), 4);
-        // Get SSE instructions availability
-        CPUID cpuID1(1, 0);
-        mIsHTT = cpuID1.EDX() & AVX_POS;
-        mIsSSE = cpuID1.EDX() & SSE_POS;
-        mIsSSE2 = cpuID1.EDX() & SSE2_POS;
-        mIsSSE3 = cpuID1.ECX() & SSE3_POS;
-        mIsSSE41 = cpuID1.ECX() & SSE41_POS;
-        mIsSSE42 = cpuID1.ECX() & SSE41_POS;
-        mIsAVX = cpuID1.ECX() & AVX_POS;
-        // Get AVX2 instructions availability
-        CPUID cpuID7(7, 0);
-        mIsAVX2 = cpuID7.EBX() & AVX2_POS;
-
-        std::string upVId = mVendorId;
-        for_each(upVId.begin(), upVId.end(), [](char &in) { in = ::toupper(in); });
-        // Get num of cores
-        if (upVId.find("INTEL") != std::string::npos) {
-            if (HFS >= 11) {
-                for (int lvl = 0; lvl < 4; ++lvl) {
-                    CPUID cpuID4(0x0B, lvl);
-                    uint32_t currLevel = (LVL_TYPE & cpuID4.ECX()) >> 8;
-                    switch (currLevel) {
-                        case 0x01:
-                            mNumSMT = LVL_CORES & cpuID4.EBX();
-                            break;
-                        case 0x02:
-                            mNumLogCpus = LVL_CORES & cpuID4.EBX();
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                mNumCores = mNumLogCpus / mNumSMT;
-            } else {
-                if (HFS >= 1) {
-                    mNumLogCpus = (cpuID1.EBX() >> 16u) & 0xFF;
-                    if (HFS >= 4) {
-                        mNumCores = 1u + (CPUID(4, 0).EAX() >> 26) & 0x3F;
-                    }
-                }
-                if (mIsHTT) {
-                    if (!(mNumCores > 1)) {
-                        mNumCores = 1;
-                        mNumLogCpus = (mNumLogCpus >= 2 ? mNumLogCpus : 2);
-                    }
-                } else {
-                    mNumCores = mNumLogCpus = 1;
-                }
-            }
-        } else if (upVId.find("AMD") != std::string::npos) {
-            if (HFS >= 1) {
-                mNumLogCpus = (cpuID1.EBX() >> 16u) & 0xFF;
-                if (CPUID(0x80000000, 0).EAX() >= 8) {
-                    mNumCores = 1u + (CPUID(0x80000008, 0).ECX() & 0xFF);
-                }
-            }
-            if (mIsHTT) {
-                if (!(mNumCores > 1)) {
-                    mNumCores = 1;
-                    mNumLogCpus = (mNumLogCpus >= 2 ? mNumLogCpus : 2);
-                }
-            } else {
-                mNumCores = mNumLogCpus = 1;
-            }
-        } else {
-            spdlog::warn("Unexpected vendor id");
-        }
-        // Get processor brand string
-        // This seems to be working for both Intel & AMD vendors
-        for (int i = 0x80000002; i < 0x80000005; ++i) {
-            CPUID cpuID(i, 0);
-            mModelName += std::string((const char *) &cpuID.EAX(), 4);
-            mModelName += std::string((const char *) &cpuID.EBX(), 4);
-            mModelName += std::string((const char *) &cpuID.ECX(), 4);
-            mModelName += std::string((const char *) &cpuID.EDX(), 4);
-        }
     }
 }
--- src/helpers/system_info.h
+++ src/helpers/system_info.h
@@ -1,96 +1,11 @@
 #ifndef BRICKSIM_SYSTEM_INFO_H
 #define BRICKSIM_SYSTEM_INFO_H
 
-#ifdef _WIN32
-
-#include <climits>
-#include <intrin.h>
 #include <string>
 #include <vector>
-
-typedef unsigned __int32 uint32_t;
-#else
-#include <stdint.h>
-#include <string>
-#include <vector>
-
-#endif
 
 namespace system_info {
-    ///https://gist.github.com/9prady9/a5e1e8bdbc9dc58b3349
-    class CPUID {
-        uint32_t regs[4];
-
-    public:
-        explicit CPUID(unsigned funcId, unsigned subFuncId);
-
-        [[nodiscard]] const uint32_t &EAX() const { return regs[0]; }
-
-        [[nodiscard]] const uint32_t &EBX() const { return regs[1]; }
-
-        [[nodiscard]] const uint32_t &ECX() const { return regs[2]; }
-
-        [[nodiscard]] const uint32_t &EDX() const { return regs[3]; }
-    };
-
-    class CPUInfo {
-    public:
-        CPUInfo();
-
-        [[nodiscard]] std::string vendor() const { return mVendorId; }
-
-        [[nodiscard]] std::string model() const { return mModelName; }
-
-        [[nodiscard]] int cores() const { return mNumCores; }
-
-        [[nodiscard]] bool isSSE() const { return mIsSSE; }
-
-        [[nodiscard]] bool isSSE2() const { return mIsSSE2; }
-
-        [[nodiscard]] bool isSSE3() const { return mIsSSE3; }
-
-        [[nodiscard]] bool isSSE41() const { return mIsSSE41; }
-
-        [[nodiscard]] bool isSSE42() const { return mIsSSE42; }
-
-        [[nodiscard]] bool isAVX() const { return mIsAVX; }
-
-        [[nodiscard]] bool isAVX2() const { return mIsAVX2; }
-
-        [[nodiscard]] bool isHyperThreaded() const { return mIsHTT; }
-
-        [[nodiscard]] int logicalCpus() const { return mNumLogCpus; }
-
-    private:
-        // Bit positions for data extractions
-        static const uint32_t SSE_POS = 0x02000000;
-        static const uint32_t SSE2_POS = 0x04000000;
-        static const uint32_t SSE3_POS = 0x00000001;
-        static const uint32_t SSE41_POS = 0x00080000;
-        static const uint32_t SSE42_POS = 0x00100000;
-        static const uint32_t AVX_POS = 0x10000000;
-        static const uint32_t AVX2_POS = 0x00000020;
-        static const uint32_t LVL_NUM = 0x000000FF;
-        static const uint32_t LVL_TYPE = 0x0000FF00;
-        static const uint32_t LVL_CORES = 0x0000FFFF;
-
-        // Attributes
-        std::string mVendorId;
-        std::string mModelName;
-        int mNumSMT;
-        int mNumCores;
-        int mNumLogCpus;
-        bool mIsHTT;
-        bool mIsSSE;
-        bool mIsSSE2;
-        bool mIsSSE3;
-        bool mIsSSE41;
-        bool mIsSSE42;
-        bool mIsAVX;
-        bool mIsAVX2;
-    };
-
-    std::vector<std::pair<const char *, std::string>> getSystemInfo();
+    std::vector<std::pair<std::string, std::string>> getSystemInfo();
 }
 
 #endif //BRICKSIM_SYSTEM_INFO_H

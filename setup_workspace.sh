03a208c7c2badaff17e1222df73fc12a28432bcf
bb1950328 <bb1950328@gmail.com>
2021-06-01 08:34:00 +0200

    cleanup setup_workspace.sh fix onlyXXbit logic

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -26,11 +26,11 @@ install_64bit_packages=true
 
 if [[ $# -ge 1 ]]
 then
-  if [[ "$1" -eq 'only32bit' ]]
+  if [ "$1" = "only32bit" ]
   then
     install_64bit_packages=false
   fi
-  if [[ "$1" -eq 'only64bit' ]]
+  if [ "$1" = "only64bit" ]
   then
     install_32bit_packages=false
   fi
@@ -43,12 +43,12 @@ if [[ "$OS" == "linux" ]]; then
   sudo apt-get install -y build-essential gcc-10 g++-10 gcc-multilib g++-multilib gcc-10-multilib g++-10-multilib \
                           cmake mesa-utils libxinerama-dev libxrandr-dev libxcursor-dev libxi-dev ninja-build
 
-  if [ "$install_32bit_packages" ]; then
+  if $install_32bit_packages; then
     sudo apt-get install -y libgl1-mesa-dev:i386 libglu1-mesa-dev:i386 freeglut3-dev:i386 libzstd-dev:i386  \
                             zlib1g-dev:i386 libssl-dev:i386 libcurl4-openssl-dev:i386 libtbb-dev:i386 \
                             libbz2-dev:i386 liblzma-dev:i386
   fi
-  if [ "$install_64bit_packages" ]; then
+  if $install_64bit_packages; then
     # there are probably more dependencies but i didn't have a 32 bit pc to try it out
     sudo apt-get install -y freeglut3-dev:amd64 libcurl4-openssl-dev:amd64 libtbb-dev:amd64
   fi
@@ -58,18 +58,17 @@ fi
 if [[ "$OS" == "windows" ]]; then
   echo "Installing packages using pacman..."
   pacman -S unzip base-devel msys2-devel libcurl-devel --noconfirm --needed
-  if [ "$install_32bit_packages" ]; then
+  if $install_32bit_packages; then
       pacman -S mingw-w64-i686-toolchain mingw-w64-i686-cmake mingw-w64-i686-freeglut --noconfirm --needed
   fi
-  if [ "$install_64bit_packages" ]; then
+  if $install_64bit_packages; then
       pacman -S mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-freeglut --noconfirm --needed
   fi
   echo "packages installed."
 fi
 
 if [[ "$OS" == "mac" ]]; then
-  which -s brew
-  if [[ $? != 0 ]] ; then
+  if ! which -s brew; then
     echo "installing homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   else

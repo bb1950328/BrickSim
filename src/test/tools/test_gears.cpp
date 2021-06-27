b0c61106ce8260a259b10e6f8ba42949e9f8dd01
bb1950328 <bb1950328@gmail.com>
2021-06-27 21:04:32 +0200

    wrap tests in bricksim:: namespace

--- src/test/tools/test_gears.cpp
+++ src/test/tools/test_gears.cpp
@@ -1,17 +1,19 @@
 #include "catch2/catch.hpp"
 #include "../../tools/gears.h"
+namespace bricksim {
+    TEST_CASE("Gear Constants") {
+        CHECK(gears::WORM_GEAR->numTeeth == 1);
+        CHECK(gears::WORM_GEAR->radiusLDU == 10);
+        CHECK(gears::WORM_GEAR->type == gears::GearType::WORM);
+    }
 
-TEST_CASE("Gear Constants") {
-    CHECK(gears::WORM_GEAR->numTeeth==1);
-    CHECK(gears::WORM_GEAR->radiusLDU==10);
-    CHECK(gears::WORM_GEAR->type==gears::GearType::WORM);
-}
+    TEST_CASE("GearPair") {
+        gears::GearPair pair(gears::WORM_GEAR, gears::GEAR_8T);
+        CHECK(pair.getDriver() == gears::WORM_GEAR);
+        CHECK(pair.getFollower() == gears::GEAR_8T);
+        CHECK(pair.getRatio() == Fraction(1, 8));
+    }
 
-TEST_CASE("GearPair") {
-    gears::GearPair pair(gears::WORM_GEAR, gears::GEAR_8T);
-    CHECK(pair.getDriver()==gears::WORM_GEAR);
-    CHECK(pair.getFollower()==gears::GEAR_8T);
-    CHECK(pair.getRatio()==Fraction(1, 8));
 }
 
 //todo more tests

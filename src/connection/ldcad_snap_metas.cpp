349b2c0bff16719ca5a14b12c45b38f3b7909d3f
bb1950328 <bb1950328@gmail.com>
2022-10-16 12:28:53 +0200

    add data structures for LDCad snap meta commands started implementing parsers

---
+++ src/connection/ldcad_snap_metas.cpp
@@ -0,0 +1,55 @@
+#include "ldcad_snap_metas.h"
+#include "../helpers/stringutil.h"
+#include "../types.h"
+#include <charconv>
+#include <vector>
+
+namespace bricksim::connection::ldcad_snap_meta {
+    Grid::Grid(std::string_view command) {
+        std::vector<std::string_view> words = stringutil::splitByChar(command, ' ');
+
+        int i = 0;
+        if (words[i] == "C") {
+            centerX = true;
+            ++i;
+        } else {
+            centerX = false;
+        }
+
+        std::from_chars(words[i].begin(), words[i].end(), countX);
+        ++i;
+        if (words[i] == "C") {
+            centerZ = true;
+            ++i;
+        } else {
+            centerZ = false;
+        }
+
+        std::from_chars(words[i].begin(), words[i].end(), countZ);
+        ++i;
+
+        std::from_chars(words[i].begin(), words[i].end(), spacingX);
+        ++i;
+
+        std::from_chars(words[i].begin(), words[i].end(), spacingZ);
+        ++i;
+    }
+
+    MetaLine Reader::readLine(std::string_view line) {
+        uomap_t<std::string_view, std::string_view> parameters;
+
+        std::size_t end=0;
+        while (true) {
+            const auto start = line.find('[', end);
+            if (start==std::string_view::npos) {
+                break ;
+            }
+            const auto middle = line.find('=', start);
+            end = line.find(']', start);
+            //todo add substrings to map
+        }
+        if (line.starts_with("SNAP_CLEAR")) {
+            //return {ClearCommand()}
+        }
+    }
+}

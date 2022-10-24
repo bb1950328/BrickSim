963aec071862ab244819d47ed0504d1b2a14ab0d
bb1950328 <bb1950328@gmail.com>
2022-10-24 22:26:28 +0200

    add data structures

--- src/connection/data.h
+++ src/connection/data.h
@@ -1,5 +1,50 @@
 #pragma once
+#include <string>
+#include <vector>
 
 namespace bricksim::connection {
+    enum class Gender {
+        M,
+        F,
+    };
 
+    class Connector {
+        std::string group;
+    };
+
+    enum class CylindricalShapeType {
+        ROUND,
+        AXLE,
+        SQUARE,
+    };
+    class CylindricalShapePart {
+    public:
+        CylindricalShapeType type;
+        bool flexibleRadius;
+        float radius;
+        float length;
+    };
+
+    class CylindricalConnector : public Connector {
+    public:
+        Gender gender;
+        std::vector<CylindricalShapePart> parts;
+        bool openStart, openEnd;
+        bool slide;
+    };
+
+    class ClipConnector : public Connector {
+    public:
+        float radius;
+        float width;
+        bool slide;
+    };
+
+    class FingerConnector : public Connector {
+        Gender firstFingerGender;
+        float radius;
+        std::vector<float> fingerWidths;
+    };
+
+    //TODO generic
 }

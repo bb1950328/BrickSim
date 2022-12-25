8dafda1132bc5e9ce605df10cbf331583df76de5
bb1950328 <bb1950328@gmail.com>
2022-12-25 22:59:05 +0100

    connector_data_provider.cpp: fix SNAP_CYL with both pos and ori

--- src/controller.cpp
+++ src/controller.cpp
@@ -396,9 +396,10 @@ namespace bricksim::controller {
         //openFile("test_files/texmap_planar_perpendicular.ldr");
         //openFile("test_files/omr/arocs.mpd");
         //openFile("test_files/omr/chiron.mpd");
-        //openFile("test_files/translated_rotated_stud.ldr");
-        openFile("3001.dat");
+        openFile("test_files/connection_info_problem_parts.ldr");
+        //openFile("3001.dat");
         //openFile("car.ldr");
+        //openFile("3626b.dat");
         //openFile("~/Downloads/datsville.ldr");
         //openFile("~/Downloads/42131_Bulldozer.mpd");
 

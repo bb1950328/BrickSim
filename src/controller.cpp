62a03e65b18ef96c5ca7bd3ee3f941b1abf61ced
bb1950328 <bb1950328@gmail.com>
2022-12-26 00:56:57 +0100

    SNAP_CYL read correctly in almost all cases

--- src/controller.cpp
+++ src/controller.cpp
@@ -396,11 +396,11 @@ namespace bricksim::controller {
         //openFile("test_files/texmap_planar_perpendicular.ldr");
         //openFile("test_files/omr/arocs.mpd");
         //openFile("test_files/omr/chiron.mpd");
-        openFile("test_files/connection_info_problem_parts.ldr");
+        //openFile("test_files/connection_info_problem_parts.ldr");
         //openFile("3001.dat");
         //openFile("car.ldr");
         //openFile("3626b.dat");
-        //openFile("~/Downloads/datsville.ldr");
+        openFile("~/Downloads/datsville.ldr");
         //openFile("~/Downloads/42131_Bulldozer.mpd");
 
         std::vector<uint64_t> lastEditorRootNodeVersions;

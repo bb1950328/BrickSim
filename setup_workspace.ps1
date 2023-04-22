0e36b373d9956fce2378aabfa4350709ad0f73dc
bb1950328 <bb1950328@gmail.com>
2023-04-22 23:23:52 +0200

    try to fix libccd dependency

--- setup_workspace.ps1
+++ setup_workspace.ps1
@@ -1,4 +1,4 @@
-vcpkg install freeglut zlib eigen3
+vcpkg install freeglut zlib eigen3 libccd
 
 Expand-Archive 'src\lib\glad.zip' 'glad_tmp'
 Move-Item -Path 'glad_tmp\src\glad.c' -Destination 'src\lib\glad.c'

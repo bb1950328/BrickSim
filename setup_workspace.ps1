c7b8a4282cdd9d58441e66a7c2926605220c024b
bb1950328 <bb1950328@gmail.com>
2023-04-22 21:09:06 +0200

    add setup script for windows

---
+++ setup_workspace.ps1
@@ -0,0 +1,6 @@
+vcpkg install freeglut zlib
+
+Expand-Archive 'src\lib\glad.zip' 'glad_tmp'
+Move-Item -Path 'glad_tmp\src\glad.c' -Destination 'src\lib\glad.c'
+Move-Item -Path 'glad_tmp\include' -Destination 'src\lib\include'
+Remove-Item -Recurse -Force 'glad_tmp'

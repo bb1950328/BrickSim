181f5340e9408eba471ff42b0b9fe9e1be412c04
bb1950328 <bb1950328@gmail.com>
2023-08-06 19:53:56 +0200

    eigen vs glm benchmark part 2

--- src/benchmarks/bench_matmul.cpp
+++ src/benchmarks/bench_matmul.cpp
@@ -1,6 +1,6 @@
+#include <Eigen/Eigen>
 #include <catch2/catch_all.hpp>
 #include <glm/glm.hpp>
-#include <Eigen/Eigen>
 
 #define GENERATE_RANDOM_VEC3()                        \
     glm::vec3 {                                       \
@@ -10,6 +10,13 @@
     }
 #define GENERATE_RANDOM_VEC4()                        \
     glm::vec4 {                                       \
+        GENERATE(take(1, random(-1.f, 1.f))),         \
+                GENERATE(take(1, random(-1.f, 1.f))), \
+                GENERATE(take(1, random(-1.f, 1.f))), \
+                GENERATE(take(1, random(-1.f, 1.f))), \
+    }
+#define GENERATE_RANDOM_EVEC4()                       \
+    Eigen::Vector4f {                                 \
         GENERATE(take(1, random(-1.f, 1.f))),         \
                 GENERATE(take(1, random(-1.f, 1.f))), \
                 GENERATE(take(1, random(-1.f, 1.f))), \
@@ -31,9 +38,14 @@ namespace bricksim {
         };
         BENCHMARK_ADVANCED("eigen")
         (Catch::Benchmark::Chronometer meter) {
-
+            Eigen::Vector4f p0 = GENERATE_RANDOM_EVEC4();
+            Eigen::Matrix4f m0;
+            m0 << GENERATE_RANDOM_EVEC4(),
+                    GENERATE_RANDOM_EVEC4(),
+                    GENERATE_RANDOM_EVEC4(),
+                    GENERATE_RANDOM_EVEC4();
             meter.measure([&] {
-                return 0;
+                return Eigen::Vector4f(m0*p0);
             });
         };
     }
